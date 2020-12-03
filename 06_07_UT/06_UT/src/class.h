7ee8ade12637bfeffd505aba8ce8960f7cd1e201
Victor <victortim2002@gmail.com>
2020-12-03 23:40:52 +0300

    Musor

---
+++ 06_07_UT/06_UT/src/class.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+#ifndef INC_06_LAB_CLASS_H
+#define INC_06_LAB_CLASS_H
+class Fraction{
+private:
+    void Check();
+    void Reduction();
+public:
+    int numerator;
+    int denominator;
+    Fraction(int A, int B);
+    Fraction (Fraction& x);
+    void DataInput();
+    int GetNum();
+    int GetDen();
+    Fraction Multiplication(Fraction factor);
+    Fraction Addition(Fraction factor);
+    Fraction Division(Fraction factor);
+    double DataOutput();
+};
+#endif
