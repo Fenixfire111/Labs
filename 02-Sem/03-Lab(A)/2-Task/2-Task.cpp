9a6c3ab166eb02c7b9f3fa37135333ded2709680
Victor <victortim2002@gmail.com>
2021-03-15 00:53:52 +0300

    Created 2-Task

---
+++ 02-Sem/03-Lab(A)/2-Task/2-Task.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter the number of elements in the arrays: " << std::endl;
+    int n;
+    std::cin >> n;
+    int* a = new int[n];
+    int* b = new int[n];
+    std::cout << "Enter the elements of array A: " << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    std::cout << "Enter the elements of array B: " << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+    }
+
+    int result[3] = { 0,0,0 };
+    __asm {
+        mov edi, a
+        mov esi, b
+        xor ecx, ecx
+        ForBegin1 :
+        mov ebx, n
+            cmp ecx, ebx
+            je ForEnd
+            mov ebx, dword ptr[edi]
+            add edi, 4
+            mov eax, dword ptr[esi]
+            add esi, 4
+            imul ebx
+            add [result], eax
+            adc [result + 4], edx
+            adc [result + 8], 0
+            inc ecx
+            jmp ForBegin1
+            ForEnd :
+    }
+
+    std::cout << result[0] << std::endl;
+    return 0;
+}
+
+
